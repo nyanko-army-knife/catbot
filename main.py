8f459d05c904425fd88788e26e35f4e029635597
draconacht <184783355+draconacht@users.noreply.github.com>
2025-04-24 00:26:37 +0530

    added permission management

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import typing
 
@@ -14,6 +15,21 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents, help_command=CustomHelpCommand())
+
+with open("catbot/assets_cache/privileges.json") as fl:
+	permissions = json.load(fl)
+
+
+@bot.check
+async def auth(ctx: discord.Message):
+	role_ids = set(role.id for role in ctx.author.roles)
+	user_id = ctx.author.id
+	channel_id = ctx.channel.id
+
+	guild_perms = permissions.get(str(ctx.guild.id))
+	if not guild_perms: return True
+	return (set(guild_perms["roles"]) and role_ids) or (user_id in guild_perms["users"]) or (
+					channel_id in guild_perms["channels"])
 
 
 @bot.command(aliases=['comboname'])
