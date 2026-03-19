f06f758d14b562019d01968ed9fc61d8990a7daa
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-19 20:47:58 +0300

    changed entity embed ui a little

--- embeds/entity.py
+++ embeds/entity.py
@@ -8,15 +8,21 @@ from .abilities import Passives
 class Entity:
 	@staticmethod
 	def embed_in(self: models.Entity, embed: discord.Embed) -> discord.Embed:
-		embed.add_field(name="Atk (DPS)", value=f'{self.atk} ({30 * self.atk / self.breakup.cd_effective:.2f})',
+		embed.add_field(name=f"<{'Area' if self.area_attack else 'Single'}> Atk (DPS)", value=f'{self.atk} ({30 * self.atk / self.breakup.cd_effective:.2f})',
 										inline=True)
 		embed.add_field(name="HP - KB Count", value=f'{self.hp:,} - {self.kb}', inline=True)
 
 		if self.breakup.hit_1 is not None:
-			embed.add_field(name="Timings", value=str(self.breakup), inline=True)
+			output = f"{self.breakup.hit_0}f"
+			if self.breakup.hit_1:
+				output += f" / {self.breakup.hit_1.after(self.hit_0)}f"
+				if self.breakup.hit_2:
+					output += f" / {self.breakup.hit_2.after(self.hit_1)}f"
+			output += f" - {self.backswing}f - {self.tba}f"
+			embed.add_field(name="Foreswing - Backswing - TBA", value=output, inline=True)
 		else:
-			embed.add_field(name="Timings",
-											value=f'↑{self.breakup.hit_0.foreswing}f / ↓{self.breakup.backswing}f / ⏲{self.breakup.tba}f',
+			embed.add_field(name="Foreswing - Backswing - TBA",
+											value=f'{self.breakup.hit_0.foreswing}f - {self.breakup.backswing}f - {self.breakup.tba}f',
 											inline=True)
 
 		display_range = f'{self.range_}'
@@ -27,9 +33,8 @@ class Entity:
 			else:
 				display_range += f' [{basehit.range_start + basehit.range_width}~{basehit.range_start}]'
 
-		embed.add_field(name="Range - Area? - Speed",
+		embed.add_field(name="Range - Speed",
 										value=f'{display_range} - '
-													f'{self.area_attack} - '
 													f'{self.speed}', inline=True)
 
 		if self.extensions or self.abilities:
